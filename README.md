8408d48560400985d2df328895341be6a35e4628
Adem Cooper <KACooper33@gmail.com>
2024-01-19 16:02:52 -0800

    Added logger using Microsoft.Extensions.Logging

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ Selenium with example using C# in a page object pattern
 Unit Testing Frameworks: https://www.lambdatest.com/blog/nunit-vs-xunit-vs-mstest/
  - Currently going with MSTest, but each has its strengths and weaknesses
 
- Page Object Model:
- This project utilizes the Page Factory in selenium to implament the Page Object Model
- For more information see: https://www.guru99.com/page-object-model-pom-page-factory-in-selenium-ultimate-guide.html
+Page Object Model:
+ - This project utilizes the Page Factory in selenium to implament the Page Object Model
+ - For more information see: https://www.guru99.com/page-object-model-pom-page-factory-in-selenium-ultimate-guide.html
 
- 
+Logging:
+- Microsoft.Extensions.Logging has been added. This implementation has limitations. The biggest being that the log itsn't output to a file. 
+
+Enhancements left to add:
+ - logging output to a file (use nlog, log4net, or serilog)
+ - Cucumber layer
+ - base classes for page and test classes that should include teardown and setup
--- src/Pages/LoginPage.cs
+++ src/Pages/LoginPage.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
 using System;
@@ -8,41 +9,57 @@ using System.Threading.Tasks;
 
 namespace PageObjectModel.Source.Pages
 {
-public class LoginPage
-{
-private IWebDriver _driver;
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+        private ILogger _logger;
 
-//***********************************************
-// MATCHERS
-//***********************************************
-[FindsBy(How = How.Name, Using = "username")]
-private IWebElement _username;
+        //***********************************************
+        // MATCHERS
+        //***********************************************
+        [FindsBy(How = How.Name, Using = "username")]
+        private IWebElement _username;
 
-[FindsBy(How = How.Name, Using = "password")]
-private IWebElement _password;
+        [FindsBy(How = How.Name, Using = "password")]
+        private IWebElement _password;
 
-[FindsBy(How = How.Name, Using = "commit")]
-private IWebElement _signIn_button;
-
-
-
-//***********************************************
-// Functions
-//***********************************************
-
-public LoginPage(IWebDriver driver) 
-{
-_driver = driver;
-PageFactory.InitElements(driver, this);
-}
-
-public void Login(string username, string password)
-{
-    _username.SendKeys(username);
-    _password.SendKeys(password);
-    _signIn_button.Click();
-}
+        [FindsBy(How = How.Name, Using = "commit")]
+        private IWebElement _signIn_button;
 
 
-}
+
+    //***********************************************
+    // Functions
+    //***********************************************
+
+    public LoginPage(IWebDriver driver) 
+    {
+        _driver = driver;
+        PageFactory.InitElements(driver, this);
+
+        // create a logger factory
+        var loggerFactory = LoggerFactory.Create(
+            builder => builder
+                // add console as logging target
+                .AddConsole()
+                // add debug output as logging target
+                .AddDebug()
+                // set minimum level to log
+                .SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Debug)
+        );
+        _logger = loggerFactory.CreateLogger<LoginPage>();
+    }
+
+    public void Login(string username, string password)
+    {
+        _logger.LogInformation($"Sending keys for Username:{username}");
+        _username.SendKeys(username);
+        _logger.LogDebug($"Sending keys for Password:{password}");
+        _password.SendKeys(password);
+        _logger.LogInformation($"Clicking Sign In Button");
+        _signIn_button.Click();
+    }
+
+
+    }
 }
